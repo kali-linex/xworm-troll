536eb8f926b0de95171de0cc751abc78a078d91f
kali-linex <87039059+kali-linex@users.noreply.github.com>
2023-04-18 23:05:20 +0200

    Begin working on a rewrite

---
+++ xworm.py
@@ -0,0 +1,56 @@
+import gzip
+import struct
+from abc import ABC, abstractmethod
+import io
+
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad
+import hashlib
+
+
+class AbstractPacket(ABC):
+    @abstractmethod
+    def write_bytes(self, into: io.IOBase):
+        ...
+
+    def get_bytes(self):
+        b = io.BytesIO()
+        self.write_bytes(b)
+        return b.getbuffer().tobytes()
+
+
+class Packet(AbstractPacket):
+    def __init__(self, *data: list[bytes]):
+        self.data = data
+
+    def write_bytes(self, into):
+        into.write(b'<Xwormmm>'.join(self.data))
+
+
+def write_all_to_stream(packets: list[AbstractPacket], key: bytes, into: io.RawIOBase):
+    key = hashlib.md5(key).digest()
+    crypto = AES.new(key, AES.MODE_ECB)
+    for p in packets:
+        encrypted = crypto.encrypt(pad(p.get_bytes(), 16))
+        into.write(str(len(encrypted)).encode('utf-8') + b'\0')
+        into.write(encrypted)
+
+
+def str_to_arg(s: str):
+    if s.startswith('gz:'):
+        with open(s[3:], 'r') as f:
+            return compress(f.read())
+    elif s.startswith('in:'):
+        with open(s[3:], 'r') as f:
+            return (f.read())
+    return s.encode('utf-8')
+
+
+def read_packet_file(stream):
+    packets = []
+    for l in stream:
+        if l.startswith()
+
+
+def compress(data):
+    return struct.pack('<L', len(data)) + gzip.compress(data)
